f160aa826db340ac605844da2611a76fd614a960
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-07-23 15:32:11 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 'use strict';
 import './sass/main.scss';
-import API from './js/apiService';
+import API from './js/api_service';
 import NOTE from './js/notifications';
 import refs from './js/refs';
