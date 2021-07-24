b2c347eb88031799c1d4dcaccc04e8a518eae7c6
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-07-24 20:30:26 +0300

    Добавил структуру проекта
    Дефолтные стили, фото, sass переменные, handlebars, debounce, класс .visually-hidden

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 'use strict';
 import './sass/main.scss';
-import API from './js/api_service';
-import NOTE from './js/notifications';
-import refs from './js/refs';
+let debounce = require('lodash.debounce');
+
+import genres from './genres.json';
+
+import btnForLibrary from './templates/btn_for_library.hbs';
+import inputHeader from './templates/input_header.hbs';
+import severalFilmCard from './templates/several_film_card.hbs';
+import oneFilmCard from './templates/one_film_card.hbs';
+// import API from './js/api_service';
+// import NOTE from './js/notifications';
+// import refs from './js/refs';
+
+// === GALLERY BLOCK
+
+// === END GALLERY BLOCK
+
+// === PAGINATION BLOCK
+
+// === END PAGINATION BLOCK
+
+// === lOCALSTORAGE BLOCK
+
+// === END lOCALSTORAGE BLOCK
