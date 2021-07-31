af214f359ed147b919f9934c9b267992c25f677a
Kateryna Burlaka <71180234+CapMcCat@users.noreply.github.com>
2021-07-31 16:53:11 +0300

    Update one_film_card.js

--- src/js/one_film_card.js
+++ src/js/one_film_card.js
@@ -31,27 +31,20 @@ function onMovieCardClick(event) {
   addEventListenerOnEscKey();
   addEventListenerOnModalBackdrop();
 
-  fetchMovieById(id)
-    .then(renderModalMovieCard)
-    .then(r => console.log(r));
+  fetchMovieById(id).then(renderModalMovieCard);
 
   refs.movieCardBackdrop.classList.remove('visually-hidden');
 }
 
 /* Закрываем модалку при клике на бэкдроп или кнопку закрытия */
 function onMovieCardBackdropClick(event) {
-  const closeTags = ['DIV', 'svg', 'use'];
-  console.log(event.target.hasAttribute('close-tag'))
+  //console.log(event.target.hasAttribute('close-tag'));
 
-  // if (!closeTags.includes(event.target.nodeName)) {
-  //   return;
-  // }
-
-    if (!event.target.hasAttribute('close-tag')) {
+  if (!event.target.hasAttribute('close-tag')) {
     return;
   }
 
-  console.log(event.target);
+  //console.log(event.target);
 
   removeEventListenerFromBackdrop();
   removeEventListenerFromEscKey();
