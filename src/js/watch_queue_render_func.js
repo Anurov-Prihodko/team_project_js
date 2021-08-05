b3bbdfcaa9c2a062c22c149e94e6796a56f1a6f2
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-08-05 09:57:47 +0300

    Отформатировалось

--- src/js/watch_queue_render_func.js
+++ src/js/watch_queue_render_func.js
@@ -1,6 +1,6 @@
 import watchAndQueueTpl from './watched_and_queue_tpl';
 import refs from './refs';
-import {fetchMovieById} from './api_service';
+import { fetchMovieById } from './api_service';
 import { realLaunch } from '../index.js';
 
 refs.buttons.addEventListener('click', onMyLibBtnsClick);
@@ -11,12 +11,12 @@ refs.homeBtn.addEventListener('click', onHomeBtnClick);
 refs.watched.addEventListener('click', watched);
 refs.queue.addEventListener('click', queue);
 
-function queue() {   
-  localStorage.setItem('position', 'queue')
+function queue() {
+  localStorage.setItem('position', 'queue');
   // console.log('queue!!!!')
 }
 function watched() {
-  localStorage.setItem('position', 'watched') 
+  localStorage.setItem('position', 'watched');
   // console.log('watched!!!!')
 }
 //mark//
@@ -28,15 +28,15 @@ function onHomeBtnClick() {
   setTimeout(() => setHomeHeight(), 200);
   correctionStyles();
   // realLaunch(1); !!!!!
-  realLaunch(Number(localStorage.getItem('home page main')))
+  realLaunch(Number(localStorage.getItem('home page main')));
 }
 
 function onMyLibClick() {
   //mark//
-  localStorage.setItem('position', 'watched')
-  refs.watchedButton.classList.add('active');  
+  localStorage.setItem('position', 'watched');
+  refs.watchedButton.classList.add('active');
   refs.queueButton.classList.remove('active');
-  //mark// 
+  //mark//
 
   refs.cardContainer.innerHTML = '';
   setTimeout(() => refs.pagination.classList.add('visually-hidden'), 200);
@@ -56,35 +56,28 @@ function onMyLibClick() {
 
 function onMyLibBtnsClick(event) {
   // localStorage.setItem('position', 'watched')
-  
-  
-  let ids = getItemsFromStorage(event.target.textContent)
-  console.log('ids = ', ids)
+
+  let ids = getItemsFromStorage(event.target.textContent);
+  console.log('ids = ', ids);
 
   if (!ids) {
-    showMyLibNotification();    
+    showMyLibNotification();
     return;
-  }
-  else  
-
-  hideMyLibNotification();
+  } else hideMyLibNotification();
   fetchMoviesOnMyLibBtnsClick(ids);
 }
 
 function fetchMoviesOnMyLibBtnsClick(idsForFetch) {
-    refs.cardContainer.innerHTML = '';
+  refs.cardContainer.innerHTML = '';
 
   const allPromises = idsForFetch.map(id => fetchMovieById(id));
 
-  Promise.all(allPromises)
-    .then(response => response.map(result => renderCardsFromStorage(result)));
+  Promise.all(allPromises).then(response => response.map(result => renderCardsFromStorage(result)));
 }
 
-
-  
 function getItemsFromStorage(key) {
   const idFromStorage = localStorage.getItem(key);
-  if (!idFromStorage) return
+  if (!idFromStorage) return;
   return idFromStorage.split(',');
 }
 
@@ -98,7 +91,7 @@ function correctionStyles() {
   refs.libButton.classList.remove('current');
   refs.homeButton.classList.add('current');
   refs.headerDom.classList.remove('lib-header');
-  refs.watchedButton.classList.add('active');  
+  refs.watchedButton.classList.add('active');
   refs.queueButton.classList.remove('active');
   setTimeout(() => refs.pagination.classList.remove('visually-hidden'), 200);
 }
@@ -120,5 +113,4 @@ function showMyLibNotification() {
 
 function hideMyLibNotification() {
   refs.notification.classList.add('visually-hidden');
-
 }
